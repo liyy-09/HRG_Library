ffe048458a437c64ac6f6acbcf261736a0ba7b97
liyy-09 <1290374862@qq.com>
2018-11-21 18:39:13 +0800

    start activity

---
+++ HRGModulePattern/moduleRIC/src/main/java/debug/StartActivity.java
@@ -0,0 +1,15 @@
+package debug;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.hrg.ric.R;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ric_activity_start);
+    }
+}
